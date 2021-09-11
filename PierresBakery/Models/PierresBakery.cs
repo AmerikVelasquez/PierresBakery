b9190be7ac7cc927964870ecfbf067b96f426fec
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-09-10 23:57:57 -0700

    add minimum code to pass test

--- PierresBakery/Models/PierresBakery.cs
+++ PierresBakery/Models/PierresBakery.cs
@@ -1,2 +1,24 @@
 using System;
 using System.Collections.Generic;
+
+namespace PierresBakery.Models
+{
+  public class Bread
+  {
+    public int BreadCost { get; set;}
+
+    public Bread(int breadCost)
+    {
+      BreadCost = breadCost;
+    }
+
+    public int price(int breadCost)
+    {
+     return BreadCost = 5;
+    }
+  }
+  // public class Pastry
+  // {
+
+  // }
+}
