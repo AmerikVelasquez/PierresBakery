bd5a0283b4eeb6ee28e1b0cb248a04ae8952441a
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-09-11 02:58:58 -0700

    add ui logic

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -11,9 +11,15 @@ namespace PierresBakery
       Console.WriteLine("Welcome to Pierre's Bakery. We sell delicious bread and pastries!!!");
       Console.WriteLine("Our prices vary but the deals we are currently advertising are Buy 2, get 1 free. A single loaf costs $5, two loaves costs $10. Pastries deals are Buy 1 for $2 or 3 for $5. Four pastries costs $7, five pastries costs $9, and six pastries costs $10.");
       Console.WriteLine("How many Loaves of bread would you like?");
-      string breadOrder = Console.ReadLine();
+      string recievedBread = Console.ReadLine();
+      int breadCost = int.Parse(recievedBread);
+      Bread breadOrder = new Bread(breadCost);
       Console.WriteLine("How many pastries would you like?");
       string pastryOrder = Console.ReadLine();
+      int pastryCost = int.Parse(pastryOrder);
+      Pastry pastryPrice = new Pastry(pastryCost);
+      int totalCost = breadFinal + pastryFinal;
+      Console.WriteLine("The total cost for the items you have chosen is" + totalCost + "dollars");
     }
   }
 }
