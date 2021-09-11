55a99d433eb77c27af9fc75ffb8f3db2302526bd
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-09-11 00:26:56 -0700

    add passing tests for all 4 options for Bread class

--- PierresBakery.Tests/ModelTests/PierresBakeryTests.cs
+++ PierresBakery.Tests/ModelTests/PierresBakeryTests.cs
@@ -1,3 +1,46 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System;
+using PierresBakery.Models;
+
+namespace PierresBakery.TestTools
+{
+  [TestClass]
+  public class BakeryTests
+  {
+    [TestMethod]
+    public void Bread_ReturnsPrice_true()
+    {
+      Bread testCost = new Bread(1);
+      Assert.AreEqual(5, testCost.price(1));
+    }
+
+    [TestMethod]
+    public void Bread_ReturnPriceIfModulo0_true()
+    {
+      Bread testCost = new Bread(3);
+      Assert.AreEqual(10, testCost.price(3));
+    }
+
+    [TestMethod]
+    public void Bread_ReturnPriceIfModulo1_true()
+    {
+      Bread testCost = new Bread(4);
+      Assert.AreEqual(15, testCost.price(4));
+    }
+
+    [TestMethod]
+    public void Bread_ReturnPriceIfModulo2_true()
+    {
+      Bread testCost = new Bread(8);
+      Assert.AreEqual(30, testCost.price(8));
+    }
+  }
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void 
+  }
+}
+
