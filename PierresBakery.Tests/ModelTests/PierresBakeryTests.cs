94ab8a78ac9824963a2d2bed49a817cd0c4ad15c
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-09-17 12:05:41 -0700

    fixed tests to reflect current naming conventions and called calculate price before AreEqual

--- PierresBakery.Tests/ModelTests/PierresBakeryTests.cs
+++ PierresBakery.Tests/ModelTests/PierresBakeryTests.cs
@@ -12,28 +12,32 @@ namespace PierresBakery.TestTools
     public void Bread_ReturnsPrice_true()
     {
       Bread testCost = new Bread(1);
-      Assert.AreEqual(5, testCost.price(1));
+      testCost.CalculatePrice();
+      Assert.AreEqual(5, testCost.Price);
     }
 
     [TestMethod]
     public void Bread_ReturnPriceIfModulo0_true()
     {
       Bread testCost = new Bread(3);
-      Assert.AreEqual(10, testCost.price(3));
+      testCost.CalculatePrice();
+      Assert.AreEqual(10, testCost.Price);
     }
 
     [TestMethod]
     public void Bread_ReturnPriceIfModulo1_true()
     {
       Bread testCost = new Bread(4);
-      Assert.AreEqual(15, testCost.price(4));
+      testCost.CalculatePrice();
+      Assert.AreEqual(15, testCost.Price);
     }
 
     [TestMethod]
     public void Bread_ReturnPriceIfModulo2_true()
     {
       Bread testCost = new Bread(8);
-      Assert.AreEqual(30, testCost.price(8));
+      testCost.CalculatePrice();
+      Assert.AreEqual(30, testCost.Price);
     }
   }
   [TestClass]
@@ -43,31 +47,36 @@ namespace PierresBakery.TestTools
     public void Pastry_ReturnPrice_true()
     {
       Pastry testPastry = new Pastry(1);
-      Assert.AreEqual(2, testPastry.price(1));
+      testPastry.CalculatePrice();
+      Assert.AreEqual(2, testPastry.Price);
     }
     [TestMethod]
     public void Pastry_ReturnPriceIfModulo0_true()
     {
       Pastry testPastry = new Pastry(3);
-      Assert.AreEqual(5, testPastry.price(3));
+      testPastry.CalculatePrice();
+      Assert.AreEqual(5, testPastry.Price);
     }
     [TestMethod]
     public void Pastry_ReturnPriceIfModulo1_true()
     {
       Pastry testPastry = new Pastry(7);
-      Assert.AreEqual(12, testPastry.price(7));
+      testPastry.CalculatePrice();
+      Assert.AreEqual(12, testPastry.Price);
     }
     [TestMethod]
     public void Pastry_ReturnPriceFor2_true()
     {
       Pastry testPastry = new Pastry(2);
-      Assert.AreEqual(4, testPastry.price(2));
+      testPastry.CalculatePrice();
+      Assert.AreEqual(4, testPastry.Price);
     }  
     [TestMethod]
     public void Pastry_ReturnPriceIfModulo2_true()
     {
       Pastry testPastry = new Pastry(11);
-      Assert.AreEqual(19, testPastry.price(11));
+      testPastry.CalculatePrice();
+      Assert.AreEqual(19, testPastry.Price);
     }
   }
 }
