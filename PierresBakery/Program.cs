738a4c78c42cd10706b5322300719638a24dfca4
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-09-17 12:07:25 -0700

    fixed naming conventions added calling the method and a way to continue the order

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -8,18 +8,36 @@ namespace PierresBakery
   {
     public static void Main()
     {
-      Console.WriteLine("Welcome to Pierre's Bakery. We sell delicious bread and pastries!!!");
-      Console.WriteLine("Our prices vary but the deals we are currently advertising are Buy 2, get 1 free. A single loaf costs $5, two loaves costs $10. Pastries deals are Buy 1 for $2 or 3 for $5. Four pastries costs $7, five pastries costs $9, and six pastries costs $10.");
+      Console.WriteLine("Welcome to Pierre's Bakery. We sell delicious bread and pastries!");
+      Console.WriteLine("Our prices vary due to the current deals that we provide.");
+      Console.WriteLine("The cost of bread is 5$ with the deal of buy 2 get 1 free.");
+      Console.WriteLine("The cost of pastries are 2$ and every 3 is 5$");
+
       Console.WriteLine("How many Loaves of bread would you like?");
-      string recievedBread = Console.ReadLine();
-      int breadCost = int.Parse(recievedBread);
+      string bread = Console.ReadLine();
+      int breadCost = int.Parse(bread);
       Bread breadOrder = new Bread(breadCost);
+      breadOrder.CalculatePrice();
+
       Console.WriteLine("How many pastries would you like?");
-      string pastryOrder = Console.ReadLine();
-      int pastryCost = int.Parse(pastryOrder);
-      Pastry pastryPrice = new Pastry(pastryCost);
-      int totalCost = breadOrder.BreadFinal + pastryPrice.PastryFinal;
-      Console.WriteLine("The total cost for the items you have chosen is" + totalCost + "dollars");
+      string pastry = Console.ReadLine();
+      int pastryCost = int.Parse(pastry);
+      Pastry pastryOrder = new Pastry(pastryCost);
+      pastryOrder.CalculatePrice();
+
+      int totalCost = breadOrder.Price + pastryOrder.Price;
+      Console.WriteLine("The total cost for the items you have chosen is" + "" + totalCost + "" + "dollars");
+      Console.WriteLine("Would you like to Change or continue your order? enter y or n");
+      string response = Console.Readline();
+      if(response = y) 
+      {
+        Main();
+      } else 
+      {
+        Console.WriteLine("it seems there was a problem completing your order. Sorry for the inconvienece but come back another day");
+        Main();
+      }
+     
     }
   }
 }
